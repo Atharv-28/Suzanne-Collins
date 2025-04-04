d4dd4cc22a7c88c87dd89915b165ad6fe62b12b0
Atharv-28 <atharvmt@outlook.com>
2025-04-04 12:34:01 +0530

    about page done

---
+++ src/Components/aboutContent.js
@@ -0,0 +1,110 @@
+import React from "react";
+import "../Styles/aboutContent.css";
+
+const AboutContent = () => {
+  return (
+    <div className="about_content">
+      <h2>Biography</h2>
+      <div className="bio_img_container">
+        <img className="bio_img" src="https://www.suzannecollinsbooks.com/images/suzannecollins-2020-photocredittoddplitt.jpg"  />      </div>
+      <p>
+        In 1991, Suzanne Collins began her professional career writing for
+        children's television. She worked on the staffs of several Nickelodeon
+        shows, including the Emmy-nominated hit Clarissa Explains it All and The
+        Mystery Files of Shelby Woo. For preschool viewers, she penned multiple
+        stories for the Emmy-nominated Little Bear and Oswald. She also co-wrote
+        the Rankin/Bass Christmas special, Santa, Baby! with her friend, Peter
+        Bakalian, which was nominated for a WGA Award in Animation. Most
+        recently she was the Head Writer for Scholastic Entertainment's
+        Clifford's Puppy Days and a freelancer on Wow! Wow! Wubbzy! While
+        working on a Kids WB show called Generation O! she met children's author
+        and illustrator James Proimos, who talked her into giving children's
+        books a try.
+      </p>
+      <p>
+        Thinking one day about Alice in Wonderland, she was struck by how
+        pastoral the setting must seem to kids who, like her own, lived in urban
+        surroundings. In New York City, you're much more likely to fall down a
+        manhole than a rabbit hole and, if you do, you're not going to find a
+        tea party. What you might find...? Well, that's the story of Gregor the
+        Overlander, the first book in her five-part fantasy war series, The
+        Underland Chronicles, which became a New York Times bestseller.
+        Internationally, it has been sold in 21 languages. Audio books in
+        English are read by Paul Boehmer and the first and the final books were
+        chosen as ALSC Notable Children's Recordings.
+      </p>
+      <p>
+        Her next series, The Hunger Games, is a dystopian war story composed of
+        five books, including the initial trilogy of The Hunger Games, Catching
+        Fire, and Mockingjay, and two prequels, The Ballad of Songbirds and
+        Snakes and Sunrise on the Reaping. It's spent over six years to date on
+        The New York Times bestseller list since publication in September 2008,
+        and has also appeared consistently on USA Today and Publishers Weekly
+        bestseller lists. An international bestseller, it has been sold in 55
+        languages. October 1, 2024, saw the publication of the first book in the
+        illustrated series with art by Nico Delort. Recent audio books in
+        English are read by Tatiana Maslany (The Hunger Games Trilogy), Santino
+        Fontana (The Ballad of Songbirds and Snakes), and Jefferson White
+        (Sunrise on the Reaping.)
+      </p>
+      <p>
+        Lionsgate released a film adaptation of THE HUNGER GAMES on March 23,
+        2012, directed by Gary Ross who also shared screenplay credit with
+        Suzanne and Billy Ray. It broke multiple box office records and went on
+        to become the 14th highest-grossing North American release of all time
+        on its way to generating nearly $700 million at the worldwide box
+        office. Lionsgate released the second installment THE HUNGER GAMES:
+        CATCHING FIRE worldwide on November 22, 2013, directed by Francis
+        Lawrence from a screenplay by Simon Beaufoy and Michael (Arndt) DeBruyn
+        and bringing back stars Jennifer Lawrence, Josh Hutcherson, Liam
+        Hemsworth, Woody Harrelson, Elizabeth Banks, Willow Shields, Paula
+        Malcomson, Donald Sutherland, Stanley Tucci and Lenny Kravitz along with
+        new cast members Philip Seymour Hoffman, Sam Claflin, Jena Malone and
+        Jeffrey Wright. It was the highest-grossing domestic box office release
+        of 2013 and the 10th highest-grossing domestic release of all time.
+        Lionsgate released THE HUNGER GAMES: MOCKINGJAY – PART 1 on November 21,
+        2014 and THE HUNGER GAMES: MOCKINGJAY – PART 2 on November 20, 2015,
+        also directed by Lawrence and welcoming Julianne Moore, Mahershala Ali,
+        Natalie Dormer, and Patina Miller to the cast. Both screenplays were by
+        Peter Craig and Danny Strong from an adaptation by Suzanne Collins. All
+        four films were produced by Nina Jacobson of Color Force and Jon Kilik.
+        On November 17, 2023, Lionsgate released THE HUNGER GAMES: THE BALLAD OF
+        SONGBIRDS AND SNAKES directed by Francis Lawrence, written by Michael
+        Lesslie and Michael Arndt, and produced by Nina Jacobson, Brad Simpson
+        and Lawrence. It starred Tom Blyth, Rachel Zegler, Viola Davis, Peter
+        Dinklage, Hunter Schafer, Fionnula Flanagan, Jason Schwartzman, and Josh
+        Andres Rivera. The worldwide box for the entire franchise is over 3.3
+        billion. THE HUNGER GAMES: SUNRISE ON THE REAPING, directed by Lawrence
+        and produced by Jacobson, Simpson and Lawrence is set to be released by
+        Lionsgate on November 20, 2026.
+      </p>
+      <p>
+        The first-ever live stage adaptation of "The Hunger Games" will begin
+        performances on Oct. 20, 2025 at the Troubadour Canary Wharf Theatre in
+        London. Adapted from the first book and the first film in the series by
+        award-winning playwright Conor McPherson, it will be helmed by renowned
+        director Matthew Dunster.
+      </p>
+      <p>
+        In September 2013, Suzanne released a critically acclaimed
+        autobiographical picture book, Year of the Jungle, illustrated by James
+        Proimos. It deals with the year she was six and her father was deployed
+        to Viet Nam. It has been sold into 12 territories in 11 languages. Her
+        first picture book, When Charlie McButton Lost Power, about a boy
+        obsessed with computer games, was illustrated by Mike Lester and came
+        out in 2005. It has been sold into 4 foreign territories.
+      </p>
+      <p>
+        In 2010, Suzanne was named to the TIME 100 list as well as the
+        Entertainment Weekly Entertainers of the Year list. In 2016, she was
+        presented with the 2016 Authors Guild Award for Distinguished Service to
+        the Literary Community for exemplifying the unique power of young
+        people's literature to change lives and create lifelong book lovers. It
+        was the first time the Guild presented the award to a YA author.
+      </p>
+      <p>Her books have sold over 100 million copies worldwide.</p>
+    </div>
+  );
+};
+
+export default AboutContent;
--- src/Components/tabs.js
+++ src/Components/tabs.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import HomeContent from "./homeContent";
+import AboutContent from "./aboutContent";
 
 const TabsComp = () => {
   const [value, setValue] = React.useState(0);
@@ -16,15 +17,17 @@ const TabsComp = () => {
         value={value}
         onChange={handleChange}
         aria-label="icon tabs example"
+        className="tabs"
       >
         <Tab value={0} label="Home" />
-        <Tab label="About" />
+        <Tab value={1} label="About" />
         <Tab label="Books & Interviews" />
         <Tab label="Audio Books" />
         <Tab label="HG Illustrated" />
         <Tab label="Educ. Resources" />
       </Tabs>
       {value === 0 && <HomeContent />}
+      {value === 1 && <AboutContent />}
     </div>
   );
 };
